c5dccab2c587addac37c40bc476dd259246766e6
jorafael3 <jalvaradoe3@gmail.com>
2024-02-20 13:01:09 -0500

    a

--- detallefactura2.php
+++ detallefactura2.php
@@ -157,117 +157,117 @@ if ($bodegaret == "" || $bodegaret == null) {
 
 
 
-// // /// ahora actualizo la table
-// $sql = "
-// UPDATE `covidsales` SET `bultos`='$bultos', `despacho`='$despacho' , `fechadesp`='$fh', `despachador`='$usuariof'  ,
-// `fechafinal`='$fecha', `usuariofinal`='$usuariof' , `estado`='$edespacho'  , `despachofinal`='$medio' , `pickup`='$pickup'
-// where factura = '$numfac' ";
-// mysqli_query($con, $sql);
+// /// ahora actualizo la table
+$sql = "
+UPDATE `covidsales` SET `bultos`='$bultos', `despacho`='$despacho' , `fechadesp`='$fh', `despachador`='$usuariof'  ,
+`fechafinal`='$fecha', `usuariofinal`='$usuariof' , `estado`='$edespacho'  , `despachofinal`='$medio' , `pickup`='$pickup'
+where factura = '$numfac' ";
+mysqli_query($con, $sql);
 
 
-// mysqli_close($con);
+mysqli_close($con);
 
-// require("conexion_mssql.php");
+require("conexion_mssql.php");
 
-// $pdo5 = new PDO("sqlsrv:server=$sql_serverName ; Database = $sql_database", $sql_user, $sql_pwd);
-// $result6 = $pdo5->prepare('{CALL [PER_Detalle_Facturas2] (?,?)}');
-// $result6->bindParam(1, $numfac, PDO::PARAM_STR);
-// $result6->bindParam(2, $bodegaFAC, PDO::PARAM_STR);
-// $MULTI = [];
-// $BODEGAS = [];
-// if ($result6->execute()) {
-// 	$result = $result6->fetchAll(PDO::FETCH_ASSOC);
-// 	foreach ($result as $row) {
-// 		if ($row["Section"] != "HEADER") {
-// 			if ($row["MULTI"] != "") {
-// 				array_push($MULTI, $row["MULTI"]);
-// 			}
-// 			if (in_array($row["BodegaID"], $BODEGAS)) {
-// 			} else {
-// 				array_push($BODEGAS, $row["BodegaID"]);
-// 			}
-// 		}
-// 	}
-// 	// var_dump($BODEGAS);
-// 	if (count($MULTI) > 0) {
+$pdo5 = new PDO("sqlsrv:server=$sql_serverName ; Database = $sql_database", $sql_user, $sql_pwd);
+$result6 = $pdo5->prepare('{CALL [PER_Detalle_Facturas2] (?,?)}');
+$result6->bindParam(1, $numfac, PDO::PARAM_STR);
+$result6->bindParam(2, $bodegaFAC, PDO::PARAM_STR);
+$MULTI = [];
+$BODEGAS = [];
+if ($result6->execute()) {
+	$result = $result6->fetchAll(PDO::FETCH_ASSOC);
+	foreach ($result as $row) {
+		if ($row["Section"] != "HEADER") {
+			if ($row["MULTI"] != "") {
+				array_push($MULTI, $row["MULTI"]);
+			}
+			if (in_array($row["BodegaID"], $BODEGAS)) {
+			} else {
+				array_push($BODEGAS, $row["BodegaID"]);
+			}
+		}
+	}
+	// var_dump($BODEGAS);
+	if (count($MULTI) > 0) {
 
-// 		$result7 = $pdo5->prepare("declare 
-// 		@facturaid varchar(10)
-// 		select @facturaid = ID from VEN_FACTURAS where Secuencia = '".$numfac."'
-// 		select estado, * from FACTURASLISTAS
-// 		where Factura = @facturaid
-// 		and estado = 'INGRESADAGUIA'
-// 		");
-// 		if ($result7->execute()) {
-// 			$res = $result7->fetchAll(PDO::FETCH_ASSOC);
-// 			// var_dump($res);
-// 			if (count($res) == count($BODEGAS)) {
-// 			} else {
-// 				$sql = "UPDATE `covidsales` SET  `estado`='Facturado'  where factura = '$numfac' ";
-// 				if (mysqli_query($con, $sql)) {
-// 					//echo "Record updated successfully";
-// 				} else {
-// 					// echo "Error updating record: " . mysqli_error($conn);
-// 				}
-// 			}
-// 		}
-// 	}
-// }
-
-
-
-
-
-
-
-
-// // /***************************************Actualiza tabla de Facturaslistas en COMPUTRONSA*************/
-// //hasta aqui la variable usuario no lleva nada
-// //echo "UsuarioF ". $usuariof."Usuario".$usuario. $base.$acceso.$bodega.$nomsuc, $comentariodesp;
-// //echo $bodegaFAC;
-
-// $tipo = 'VEN-FA';
-// $pdo5 = new PDO("sqlsrv:server=$sql_serverName ; Database = $sql_database", $sql_user, $sql_pwd);
-// $result5 = $pdo5->prepare('Log_facturaslistas_despacho_update  @guia =:despacho ,@bultos=:bultos,  @transporte=:medio,
-// 							   @entregado= :usuario ,@numfac=:numfac, @tipo=:tipo, @comentariod=:comentariod , @bodegaFAC=:bodegaFAC');
-// $result5->bindParam(':despacho', $despacho, PDO::PARAM_STR);
-// $result5->bindParam(':bultos', $bultos, PDO::PARAM_STR);
-// $result5->bindParam(':medio', $medio, PDO::PARAM_STR);
-// $result5->bindParam(':usuario', $usuariof, PDO::PARAM_STR);
-// $result5->bindParam(':numfac', $numfac, PDO::PARAM_STR); // ENVIO SECUENCIA DE FACTURA
-// $result5->bindParam(':tipo', $tipo, PDO::PARAM_STR);
-// $result5->bindParam(':comentariod', $comentariodesp, PDO::PARAM_STR);
-// $result5->bindParam(':bodegaFAC', $bodegaFAC, PDO::PARAM_STR);
-// $result5->execute();
+		$result7 = $pdo5->prepare("declare 
+		@facturaid varchar(10)
+		select @facturaid = ID from VEN_FACTURAS where Secuencia = '".$numfac."'
+		select estado, * from FACTURASLISTAS
+		where Factura = @facturaid
+		and estado = 'INGRESADAGUIA'
+		");
+		if ($result7->execute()) {
+			$res = $result7->fetchAll(PDO::FETCH_ASSOC);
+			// var_dump($res);
+			if (count($res) == count($BODEGAS)) {
+			} else {
+				$sql = "UPDATE `covidsales` SET  `estado`='Facturado'  where factura = '$numfac' ";
+				if (mysqli_query($con, $sql)) {
+					//echo "Record updated successfully";
+				} else {
+					// echo "Error updating record: " . mysqli_error($conn);
+				}
+			}
+		}
+	}
+}
 
 
 
 
 
 
-//Para enviar el correo dependiendo de lo q haya seleccionado como forma de Despacho en la Guia
 
-// if ($medio == 'Casillero') {
-// 	echo "<h2>Use este código para abrir el casillero cuando vaya a cargar la orden:</h2>";
-// 	echo "<img src=\"https://chart.googleapis.com/chart?chs=400x400&cht=qr&chl=" . $hash5 . "&choe=UTF-8\" title=\"Computron\" /><br>";
-// 	echo "Factura: " . $numfac . "<br>";
-// 	echo "Casillero: " . $radio[0] . " del COMPUTRON " . $local . "<br>";
-// 	echo "<b><u>Nota:</u></b> Este código es válido para 1 solo uso.<br>";
-// 	echo "<br>Imprima este documento y tengalo junto con la mercadería <br>";
-// 	echo "Necesitará este código para poder abrir el casillero <br>";
-// }
-// if ($bodegaFAC == '0500000003' or $bodegaFAC == '0500000005' or $bodegaFAC == '0500000006' or $bodegaFAC == '0500000007') //$bodegaFAC =='0500000002' oror $bodegaFAC =='0500000008') //Si la bodega de facturacion es MABE,ECOLINE, BOYACA,LYCAN o CONSIGNACION CARTIMEX debe enviar correo al Proveedor or $bodegaFAC =='0500000004'
-// {
-// 	header("Refresh: 0 ; mailproveedor.php");
-// } else {
-// 	if (($medio == 'Urbano') or ($medio == 'Tramaco') or ($medio == 'Servientrega') or ($medio == 'Vehiculo Computron')) {
-// 		//echo "Entra aqui ";
-// 		header("Refresh: 0 ; mailcourier.php");
-// 	}
-// 	if ($medio == 'Entrega en tienda') {
-// 		header("location: mailretiroenotratienda.php");
-// 	}
-// }
+
+// /***************************************Actualiza tabla de Facturaslistas en COMPUTRONSA*************/
+//hasta aqui la variable usuario no lleva nada
+//echo "UsuarioF ". $usuariof."Usuario".$usuario. $base.$acceso.$bodega.$nomsuc, $comentariodesp;
+//echo $bodegaFAC;
+
+$tipo = 'VEN-FA';
+$pdo5 = new PDO("sqlsrv:server=$sql_serverName ; Database = $sql_database", $sql_user, $sql_pwd);
+$result5 = $pdo5->prepare('Log_facturaslistas_despacho_update  @guia =:despacho ,@bultos=:bultos,  @transporte=:medio,
+							   @entregado= :usuario ,@numfac=:numfac, @tipo=:tipo, @comentariod=:comentariod , @bodegaFAC=:bodegaFAC');
+$result5->bindParam(':despacho', $despacho, PDO::PARAM_STR);
+$result5->bindParam(':bultos', $bultos, PDO::PARAM_STR);
+$result5->bindParam(':medio', $medio, PDO::PARAM_STR);
+$result5->bindParam(':usuario', $usuariof, PDO::PARAM_STR);
+$result5->bindParam(':numfac', $numfac, PDO::PARAM_STR); // ENVIO SECUENCIA DE FACTURA
+$result5->bindParam(':tipo', $tipo, PDO::PARAM_STR);
+$result5->bindParam(':comentariod', $comentariodesp, PDO::PARAM_STR);
+$result5->bindParam(':bodegaFAC', $bodegaFAC, PDO::PARAM_STR);
+$result5->execute();
+
+
+
+
+
+
+Para enviar el correo dependiendo de lo q haya seleccionado como forma de Despacho en la Guia
+
+if ($medio == 'Casillero') {
+	echo "<h2>Use este código para abrir el casillero cuando vaya a cargar la orden:</h2>";
+	echo "<img src=\"https://chart.googleapis.com/chart?chs=400x400&cht=qr&chl=" . $hash5 . "&choe=UTF-8\" title=\"Computron\" /><br>";
+	echo "Factura: " . $numfac . "<br>";
+	echo "Casillero: " . $radio[0] . " del COMPUTRON " . $local . "<br>";
+	echo "<b><u>Nota:</u></b> Este código es válido para 1 solo uso.<br>";
+	echo "<br>Imprima este documento y tengalo junto con la mercadería <br>";
+	echo "Necesitará este código para poder abrir el casillero <br>";
+}
+if ($bodegaFAC == '0500000003' or $bodegaFAC == '0500000005' or $bodegaFAC == '0500000006' or $bodegaFAC == '0500000007') //$bodegaFAC =='0500000002' oror $bodegaFAC =='0500000008') //Si la bodega de facturacion es MABE,ECOLINE, BOYACA,LYCAN o CONSIGNACION CARTIMEX debe enviar correo al Proveedor or $bodegaFAC =='0500000004'
+{
+	header("Refresh: 0 ; mailproveedor.php");
+} else {
+	if (($medio == 'Urbano') or ($medio == 'Tramaco') or ($medio == 'Servientrega') or ($medio == 'Vehiculo Computron')) {
+		//echo "Entra aqui ";
+		header("Refresh: 0 ; mailcourier.php");
+	}
+	if ($medio == 'Entrega en tienda') {
+		header("location: mailretiroenotratienda.php");
+	}
+}
 
 //Variables de sesion de SGL
 $_SESSION['usuario'] = $usuariof;
